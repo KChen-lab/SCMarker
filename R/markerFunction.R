9862b65fa4a149cc90d58c2160691c43786a72db
Fang Wang <wfang0828@gmail.com>
2018-09-04 17:21:14 -0500

    update

--- R/markerFunction.R
+++ R/markerFunction.R
@@ -3,6 +3,12 @@
 # Author: FWang9
 ###############################################################################
 
+library(SingleCellExperiment)
+library(SC3)
+library(scater)
+library(dplyr)
+library(pheatmap)
+library(Seurat)
 
 ####preprocess data
 ##count expression cell or gene
@@ -32,7 +38,7 @@ peak<-function(pp){
 	return(Hpeak)
 }
 ###number of peak
-genepeak<-function(x,width){
+genepeak<-function(x,width=1){
 	if (length(x[x!=0])>1){
 		pp=density(x[x!=0],width=width)
 		Hpeak=peak(pp)
@@ -43,11 +49,12 @@ genepeak<-function(x,width){
 }
 
 #####filter data
-ModalFilter<-function(data,geneK,cellK,width,cutoff){
+ModalFilter<-function(data,geneK,cellK,width = 1,cutoff = 2){
+	rawdata=data
 	cellSumm=data.frame(cell=colnames(data))
 	cellSumm$count<-sapply(1:dim(data)[2],genecount,data=data,index="col")
 	data=data[,cellSumm$count>cellK]
-	binadata=Bina(data,cutoff=2)
+	binadata=Bina(data,cutoff=cutoff)
 	geneSumm=data.frame(gene=row.names(data))
 	geneSumm$count<-rowSums(binadata)
 	data=data[geneSumm$count>geneK,]
@@ -58,32 +65,33 @@ ModalFilter<-function(data,geneK,cellK,width,cutoff){
 	data=data[geneSumm$exppeak>=2,]
 	binadata=binadata[geneSumm$exppeak>=2,]
 	geneSumm=geneSumm[geneSumm$exppeak>=2,]
-	filterres=list(data=data,geneSumm=geneSumm,cellSumm=cellSumm,binadata=binadata)
-	return(filterres)
+	obj=list(rawdata=rawdata,newdata=data,geneSumm=geneSumm,cellSumm=cellSumm,binadata=binadata)
+	return(obj)
 }
 #####Intial marker
-GeneFilter<-function(filterres,maxexp){
+GeneFilter<-function(obj){
+	maxexp=dim(obj$newdata)[2]*0.8
 	data(excludeGene)
 	excludeGene=as.character(excludeGene[,1])
-	geneSumm=filterres$geneSumm
+	geneSumm=obj$geneSumm
 	index=match(geneSumm$gene,excludeGene)
 	geneSumm=geneSumm[is.na(index),]
-	data=filterres$data
+	data=obj$newdata
 	index=match(geneSumm$gene,row.names(data))
 	data=data[index,]
-	binadata=filterres$binadata
+	binadata=obj$binadata
 	binadata=binadata[index,]
 	geneindex=rowSums(binadata)
 	binadata=binadata[geneindex<maxexp,]
 	data=data[geneindex<maxexp,]
 	geneSumm=geneSumm[geneindex<maxexp,]
-	filterres$data=data
-	filterres$geneSumm=geneSumm
-	filterres$binadata=as.matrix(binadata)
-	return(filterres)
+	obj$newdata=data
+	obj$geneSumm=geneSumm
+	obj$binadata=as.matrix(binadata)
+	return(obj)
 }
 ######Inial cluster
-Bina<-function(data,cutoff){
+Bina<-function(data,cutoff=2){
 	data[data<cutoff]=0
 	data[data>=cutoff]=1
 	return(data)
@@ -118,9 +126,9 @@ getMEN<-function(HamDD,genename,MNN,MNNIndex){
 	genePair=unique(as.character(do.call(cbind,lapply(1:length(MNNgene),MNNpair,MNNgene=MNNgene,geneName=genename))))
 	return(genePair)
 }
-getMarker<-function(filterres,MNN,MNNIndex){
-	data=filterres$data
-	binadata=filterres$binadata
+getMarker<-function(obj,MNN,MNNIndex){
+	data=obj$newdata
+	binadata=obj$binadata
 	genename=row.names(binadata)
 	geneindex=rowSums(binadata)
 	HamD=tcrossprod(binadata)
@@ -128,8 +136,101 @@ getMarker<-function(filterres,MNN,MNNIndex){
 	HamDD=tcrossprod((1-binadata),binadata)
 	MNNmarker=getMNN(HamD=HamD,genename=genename,MNN=MNN,MNNIndex=MNNIndex)
 	MENmarker=getMEN(HamD=HamDD,genename=genename,MNN=MNN,MNNIndex=MNNIndex)
-	filterres$marker=union(MNNmarker,MENmarker)
-	return(filterres)
+	obj$marker=union(MNNmarker,MENmarker)
+	return(obj)
+}
+
+######cluster
+SCcluster<-function(obj){
+	data=obj$rawdata
+	marker=obj$marker
+	pbmc <- CreateSeuratObject(raw.data = data, min.cells = 3, min.genes = 200, 
+	                           project = "project")
+	pbmc <- NormalizeData(object = pbmc, normalization.method = "LogNormalize", 
+	                      scale.factor = 10000)
+	pbmc <- FindVariableGenes(object = pbmc, mean.function = ExpMean, dispersion.function = LogVMR, 
+	                          x.low.cutoff = 0.0125, x.high.cutoff = 3, y.cutoff = 0.5)
+	pbmc <- ScaleData(object = pbmc)
+	pbmc <- RunPCA(object = pbmc, pc.genes = gene, do.print = TRUE, pcs.print = 1:5, 
+	                genes.print = 5)
+	pbmc <- FindClusters(object = pbmc, reduction.type = "pca", dims.use = 1:8, 
+	                      resolution = 0.6, print.output = 0, save.SNN = TRUE,force.recalc=TRUE)
+	pbmc <- RunTSNE(object = pbmc, dims.use = 1:15, do.fast = TRUE)
+	TSNE=pbmc@dr$tsne@cell.embeddings
+	seuratCluster=pbmc@ident
+	dbscanCluster=dbscan(TSNE,eps=1.2,minPts=15)$cluster
+	names(dbscanCluster)=row.names(TSNE)
+	obj$tsne=TSNE
+	obj$seuratCluster=seuratCluster
+	obj$dbscanCluster=dbscanCluster
+	return(obj)
 }
 
 
+
+#####cluster-specific marker
+getClusterGene<-function(obj,method){
+	marker=obj$marker
+	if (method=="Seurat"){
+		seuratCluster=obj$seuratCluster
+		gene=get_marker_genes(obj$rawdata[marker,names(seuratCluster)], seuratCluster)
+		gene$gene=marker
+		obj$clustergene=gene
+		obj$method=method
+	}else if (method == "dbscan"){
+		dbscanCluster=obj$dbscanCluster
+		gene=get_marker_genes(obj$rawdata[marker,names(dbscanCluster)], dbscanCluster)
+		gene$gene=marker
+		obj$clustergene=gene
+		obj$method=method
+	} 
+	return(obj)
+}
+
+HeatmapCluster<-function(obj,top,scale="none"){
+	feature=obj$clustergene
+	topmarker=as.data.frame(feature %>% group_by(clusts) %>% top_n(top, auroc))
+	topmarker=topmarker[order(topmarker$clusts),]
+	method=obj$method
+	if (method=="Seurat"){
+		cluster=obj$seuratCluster
+	}else if (method=="dbscan"){
+		cluster=obj$dbscanCluster
+	}
+	clustertype=levels(factor(cluster))
+  	genemean=c()
+  	for (i in clustertype){
+    	cell=names(cluster[cluster==i])
+    	subdata=obj$rawdata[topmarker$gene,cell]
+    	genemean=cbind(genemean,apply(subdata,1,mean))
+  	}
+  	row.names(genemean)=topmarker$gene
+  	colnames(genemean)=clustertype
+  	
+  	mat_col <- data.frame(cluster=levels(factor(cluster)))
+	row.names(mat_col) <- levels(factor(cluster))
+	mat_colors <- list(cluster=rainbow(length(clustertype)))
+	names(mat_colors$cluster)=levels(factor(cluster))
+  	pheatmap(genemean,cluster_cols = F,scale=scale,cluster_rows = F,show_colnames=F,annotation_col = mat_col,annotation_colors = mat_colors)
+}
+HeatmapCell<-function(obj,top){
+	feature=obj$clustergene
+	topmarker=as.data.frame(feature %>% group_by(clusts) %>% top_n(top, auroc))
+	topmarker=topmarker[order(topmarker$clusts),]
+	method=obj$method
+	if (method=="Seurat"){
+		cluster=obj$seuratCluster
+		cluster=cluster[order(cluster)]
+	}else if (method=="dbscan"){
+		cluster=obj$dbscanCluster
+		cluster=cluster[order(cluster)]
+	}
+	clustertype=unique(cluster)
+	data=obj$rawdata[topmarker$gene,names(cluster)]
+	clustercount=table(cluster)
+	gapIndex=c()
+	for (i in 2:(length(clustertype)-1)){
+		gapIndex=c(gapIndex,sum(clustercount[1:i]))
+	}
+	pheatmap(data,cluster_cols = F,cluster_rows = F,show_colnames=F,gaps_col=gapIndex)
+}
--- README.md
+++ README.md
@@ -1,52 +1,35 @@
 # SCMarker
 
-SCMarker is a R package that performs ab initio marker selection from single cell RNA sequenceing data to enhance downstream cell-type clustering, trajectory inference and cell-type specific biological analysis.
+SCMarker is about marker selection on single cell RNA sequenceing data. This package implement the marker selection algorithm developed by Fang Wang. It provides the user with tools for generating features to further clustering. This is done based on two hypotheses. One is that gene should follow bi/multi-modal distribution in a mixed cell population if it is a marker of a specific cell type. The second is that genes which are the markers of the same cell type should co-express in the same cells.
 
-SCMarker tests two properties of a scRNAseq dataset:
-
-1. A gene expression level should follow bi/multi-modal distribution in a mixed cell population if it is a marker of a specific cell type.
-
-2. Marker genes of the same cell type tend to be co-expressed in the same cells and mutually exclusive with genes which are markers of different cell type.
-
-SCMarker is written by Fang Wang (fwang9@mdanderson.org) in Ken Chen' lab at the Department of Bioinformatics and Computational Biology in the MD Anderson Cancer Center
 
 
 Marker selection
 ---------------------
-SCMarker contains three main functions:
-
-1. `ModalFilter()`,
-    performs the initial filtering based on the least expressed number of genes (cells) and whether the gene is unimodal distribution.
-
-2. `GeneFilter()`
-    takes the output of `ModalFilter()` and filtered the genes which are unimodal distribution and express in more than maxexp cells.
-
-3. `getMarker()`.
-    takes the output of `GeneFilter()` and selects the final markers based on mutually coexpressed gene pairs.
+The three main functions for this package are `ModalFilter()`, `GeneFilter()` and `getMarker()`. The first does the initial filter based on the least expressed number of genes(cells) and whether the gene is unimodal distribution. The second takes the output of `ModalFilter()` and filtered the genes which are unimodal distribution and express in more than maxexp cells. The last takes the output of `geneFilter()` and selects the final markers based on gene pairs which are mutual maximally co-expressed in cells.
 
 
 
 Usage
 ----------------------
-You can install SCMarker from GitHub
 ```
-library(devtools)
-install_github("fang0828/SCMarker")
-```
-Alternatively, you can download from GitHub
-```
-#https://github.com/Fang0828/SCMarker/releases
-#Download SCMarker_1.1.tar.gz
-install.packages("SCMarker_1.1.tar.gz",repos=NULL,type="source")
+Download SCMarker_2.0.tar.gz
 ```
 
 ```R
-library(SCMarker)
+install.packages("SCMarker_2.0.tar.gz",repos=NULL,type="source")
 data(melanoma)
 melanoma1=as.matrix(melanoma[,2:dim(melanoma)[2]])
 row.names(melanoma1)=melanoma[,1]
-filterres=ModalFilter(data=melanoma1,geneK=10,cellK=10,width=2,cutoff=2)
-filterres=GeneFilter(filterres=filterres,maxexp=dim(melanoma1))
-filterres=getMarker(filterres=filterres,MNN=200,MNNindex=20)
-filterres$marker
+res=ModalFilter(data=melanoma1,geneK=10,cellK=10,width=2)# default width = 1 for UMI data, width =2 for TPM data.
+res=GeneFilter(obj=res)
+res=getMarker(obj=res,MNN=300,MNNIndex=30)
+library(Seurat)
+library(SC3)
+library(dbscan)
+library(dplyr)
+res=SCcluster(obj=res)
+res=getClusterGene(obj=res,method="Seurat")
+HeatmapCluster(obj=res,top=10)
+HeatmapCell(obj=res,5)
 ```
